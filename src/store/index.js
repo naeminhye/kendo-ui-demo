667d1e1c16d00ddbbfd0336431dc353202ee5a72
hieu.nguyen <hieu.nguyen@metanet.co.kr>
2020-10-27 14:05:19 +0700

    Update store and package version

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,27 @@ import createSagaMiddleware from 'redux-saga';
 
 const saga = createSagaMiddleware();
 
-const store = compose(
-  window.devToolsExtension && window.devToolsExtension(),
-)(createStore)(rootReducer);
 
+let composeEnhancers = compose;
+
+// If Redux Dev Tools and Saga Dev Tools Extensions are installed, enable them
+/* istanbul ignore next */
+if (process.env.NODE_ENV !== 'production' && typeof window === 'object') {
+  /* eslint-disable no-underscore-dangle */
+  if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({});
+
+  // NOTE: Uncomment the code below to restore support for Redux Saga
+  // Dev Tools once it supports redux-saga version 1.x.x
+  // if (window.__SAGA_MONITOR_EXTENSION__)
+  //   reduxSagaMonitorOptions = {
+  //     sagaMonitor: window.__SAGA_MONITOR_EXTENSION__,
+  //   };
+  /* eslint-enable */
+}
+
+
+const store = createStore(rootReducer, undefined, composeEnhancers)
 // saga.run()
 
 export default store;
